1079afc2c5637de42e506f5daa3b1d33bb8683e8
Martin Rist <mlr-github@martinrist.com>
2014-11-16 17:28:27 +0000

    Solution to p28 - including tree-seq variant suggested by user 4clojure1

--- src/offline_4clojure/p28.clj
+++ src/offline_4clojure/p28.clj
@@ -5,9 +5,25 @@
 (ns offline-4clojure.p28
   (:use clojure.test))
 
+; My solution
 (def __
-;; your solution here
+  (fn flattify [coll]
+    (mapcat (fn [member]
+              (if (coll? member)
+                (if (every? (complement coll?) member)
+                  member
+                  (flattify member))
+                (vector member))) coll))
 )
+
+
+; 4clojure1's solution, using tree-seq
+; Note use of sequential? rather than seq otherwise we lose the vectors.
+(def __
+  (fn my-flatten [x]
+    (filter (complement sequential?)
+            (rest (tree-seq sequential? seq x))))
+  )
 
 (defn -main []
   (are [soln] soln
